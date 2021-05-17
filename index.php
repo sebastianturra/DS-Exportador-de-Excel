e2c29ee4ee46c0391c0f3ee654de97dc726caa52
Sebastian <sebastianturra@gmail.com>
2021-05-17 10:57:14 -0400

    subida 17.05.2021

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ for ($i=2;$i<=$filas;$i++){
                   , `epru_numfolio`) VALUES ";
                   $sql.="('".$_DATOS_EXCEL[$i]['epru_id']."','";
                   $sql.=$_DATOS_EXCEL[$i]['epru_nom']."','";
-                  $sql.=$_DATOS_EXCEL[$i]['epru_fecnac'].")";  //ACA ESTÁ EL
+                  $sql.=$_DATOS_EXCEL[$i]['epru_fecnac'].")";  //ACA ESTÁ EL codigo
         }
   
                         echo $sql;
